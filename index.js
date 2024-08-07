6651d4ea76d6a05567732669fbb4b7305e553222
ClelieG <167385095+ClelieG@users.noreply.github.com>
2024-08-07 10:38:24 -0500

    routes and port added

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const express = require('express')
 const client = new pg.Client(process.env.DATABASEURL || 'postgres://postgres:molly@localhost:5432/the_acme_workshop_db')
 const app = express()
 
+// routes
+app.use(express.json());
+app.use(require('morgan')('dev'));
+
+app.post('/api/flavors', async(req, res, next) =>{})
+
+app.get('/api/flavors', async(req, res, next) =>{})
+
+app.get('/api/flavors/:id', async(req, res, next) =>{})
+
+app.put('/api/flavors', async (req, res, next) =>{})
+
+app.delete('/api/flavors/:id', async (req, res, next) =>{})
+
+//
 
 const init = async () => {
     await client.connect();
@@ -20,9 +35,16 @@ const init = async () => {
         `;
     await client.query(SQL);
         console.log('tables created');
-    SQL = ``;
+    SQL = `
+    INSERT INTO flavors(name, is_favorite) VALUES ('Vanilla', true);
+    INSERT INTO flavors(name, is_favorite) VALUES ('Strawberry', false);
+    INSERT INTO flavors(name, is_favorite) VALUES ('Caramel', false);
+    INSERT INTO flavors(name) VALUES('create routes');
+    `;
     await client.query(SQL);
         console.log('Data seeded');
+    const PORT = process.env.PORT || '3000'
+    app.listen(PORT, () => console.log(`listening on port ${PORT}`))
 }
 
 init()
